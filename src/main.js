f54e380b18bde6b910908e058ffb89fe0e92d2f4
imhta <imhtapm@gmail.com>
2019-10-02 23:45:39 +0530

    added bold mark down option

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 $("textarea").after(`<div class="mdeditor-toolbar"></div>`);
-
-var ele = `<button name="bold">B</button>
-           <button name="italic"><i>I</i></button>
-           <button name="strike"><s>S</s></button>
-           <button name="h1">H1</button>
-           <button name="h2">H2</button>
-           <button name="h3">H3</button>
-           <button name="unorderedlist">⚫</button>
-           <button name="orderedlist">1.</button>
-           <button name="checklist">✔</button>
-           <button name="blockquote">""</button>
-           <button name="inlinecode">I<></button>
-           <button name="blockcode"><>B</button>
-           <button name="link">A</button>
+var ele = `<button type="button" name="bold">B</button>
+           <button type="button" name="italic"><i>I</i></button>
+           <button type="button" name="strike"><s>S</s></button>
+           <button type="button" name="h1">H1</button>
+           <button type="button" name="h2">H2</button>
+           <button type="button" name="h3">H3</button>
+           <button type="button" name="unorderedlist">⚫</button>
+           <button type="button" name="orderedlist">1.</button>
+           <button type="button" name="checklist">✔</button>
+           <button type="button" name="blockquote">" "</button>
+           <button type="button" name="inlinecode">I<></button>
+           <button type="button" name="blockcode"><>B</button>
+           <button type="button" name="link">A</button>
            `
 $('.mdeditor-toolbar').append(ele)
 
 $('.mdeditor-toolbar button').click(function () {
-    alert($(this).attr('name'))
+    switch($(this).attr('name')) {
+      case 'bold':
+        $('textarea').val(function() {return this.value + "**"})
+        break;
+    }
 })
