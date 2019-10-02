ac5b916fe3bb8889596ff9cb913fd9b06c0d9589
imhta <imhtapm@gmail.com>
2019-10-02 23:17:57 +0530

    added toolbar +  buttons with name atrr

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
+$("textarea").after(`<div class="mdeditor-toolbar"></div>`);
 
-var $curFocusEle = ""
-$( "textarea" ).focus(function () {
-  $('#mdeditor-toolbar').remove()
-  $(this).after(`<div id="mdeditor-toolbar"></div>`)
-  });
+var ele = `<button name="bold">B</button>
+           <button name="italic"><i>I</i></button>
+           <button name="strike"><s>S</s></button>
+           <button name="h1">H1</button>
+           <button name="h2">H2</button>
+           <button name="h3">H3</button>
+           <button name="unorderedlist">⚫</button>
+           <button name="orderedlist">1.</button>
+           <button name="checklist">✔</button>
+           <button name="blockquote">""</button>
+           <button name="inlinecode">I<></button>
+           <button name="blockcode"><>B</button>
+           <button name="link">A</button>
+           `
+$('.mdeditor-toolbar').append(ele)
 
+$('.mdeditor-toolbar button').click(function () {
+    alert($(this).attr('name'))
+})
